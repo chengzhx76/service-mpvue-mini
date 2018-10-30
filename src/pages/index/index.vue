7ced905f6fdafcc9d9263dbe365da179cb89c7b6
chengzhx76 <chengzhx76@qq.com>
2018-10-30 23:07:06 +0800

    增加首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,16 +3,37 @@
 
     <view class="main">
       <view class="section-swiper">
-        <swiper :indicator-dots="indicatorDots" :autoplay="autoplay" :circular="circular"
+        <swiper class="swiper-main" :indicator-dots="indicatorDots" :autoplay="autoplay" :circular="circular"
                 :vertical="vertical" :interval="interval" :duration="duration">
-          <swiper-item v-for="item in imgs" :key="item.id">
-            <view class="swiper-item">
-              <image class="swiper-img" :src="item.src"/>
-            </view>
+          <swiper-item class="swiper-item" v-for="item in imgs" :key="item.id">
+            <image class="swiper-img" mode="scaleToFill" :src="item.src"/>
           </swiper-item>
         </swiper>
       </view>
+      <view class="search">
+        <view class="search-main">
 
+          <view class="service">
+
+            <view class="origin info">
+              <view class="title">
+                <view class="icon">
+                  <text class="fa fa-car gray-icon"/>
+                </view>
+                <text class="label">起点</text>
+              </view>
+              <view class="input">
+                <input placeholder-class="placeholder-color" placeholder="出发地"/>
+              </view>
+            </view>
+
+          </view>
+          <view class="search-btn">
+
+          </view>
+
+        </view>
+      </view>
 
     </view>
     <view class="add">
@@ -35,11 +56,15 @@ export default {
       imgs: [
         {
           id: '1',
-          src: 'https://dummyimage.com/320x150/4A7BF7&text=Hello'
+          src: 'https://dummyimage.com/240x150/50B347&text=Hello1'
         },
         {
           id: '2',
-          src: 'https://dummyimage.com/320x150/4A7BF7&text=Hello'
+          src: 'https://dummyimage.com/240x150/4A7BF7&text=Hello2'
+        },
+        {
+          id: '3',
+          src: 'https://dummyimage.com/240x150/894FC4&text=Hello3'
         }
       ],
       indicatorDots: true,
@@ -63,5 +88,47 @@ export default {
 }
 </script>
 
-<style scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  #index {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .main {
+    height: 320rpx;
+    width: 100%;
+    .section-swiper {
+      height: 100%;
+      width: 100%;
+      margin-bottom: 20rpx;
+      .swiper-main {
+        height: 100%;
+        width: 100%;
+        .swiper-item {
+          height: 100%;
+          width: 100%;
+          .swiper-img {
+            height: 100%;
+            width: 100%;
+          }
+        }
+      }
+    }
+    .search {
+      height: 300rpx;
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      background: darkgoldenrod;
+      .search-main {
+        height: 100%;
+        width: 94%;
+        background: darkgrey;
+      }
+    }
+  }
 </style>
