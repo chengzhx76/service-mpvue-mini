635e76103cb9d4827849a418c3b9234fbc1bc77d
chengzhx76 <chengzhx76@qq.com>
2018-11-04 16:55:07 +0800

    修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,10 +11,11 @@
         </swiper>
         <view class="user-main">
           <view class="switch">
-            <view class="passenger btn active">乘客</view>
-            <view class="driver btn">司机</view>
+            <view class="all btn active">全部</view>
+            <view class="passenger btn">乘客</view>
+            <view class="driver btn">车主</view>
           </view>
-          <view class="my">我的</view>
+          <view class="my">个人中心</view>
         </view>
       </view>
       <view class="search">
@@ -45,54 +46,177 @@
 
           </view>
           <view class="search-btn">
-            <button class="search-button" type="default" size="default" @click="handler">搜索</button>
+            <button class="search-button" type="default" size="default" @click="handler">搜索顺路车程</button>
           </view>
 
         </view>
       </view>
 
     </view>
-    <view class="list">
+    <scroll-view class="list" scroll-y>
+    <!--<view class="list">-->
+
       <view class="cart">
-        <view class="map">
-          <view class="run">
-            <view class="origin address">
-              <view class="icon">
-                <text class="fa fa-car gray-icon"/>
-              </view>
-              <text class="text">菏泽</text>
-            </view>
-            <view class="destination address">
-              <view class="icon">
-                <text class="fa fa-car gray-icon"/>
-              </view>
-              <text class="text">成武</text>
-            </view>
-          </view>
-          <view class="info">
-            <view class="price">
-              <text class="money">12</text>
-              <text class="unit">/人</text>
-            </view>
-            <view class="note">8小时后</view>
+        <view class="header">
+          <view class="nav-block"></view>
+          <view class="nav-main">
+            <view class="tag">车找人</view>
+            <view class="seats">剩余3位座</view>
           </view>
         </view>
-        <view class="guild">
-          <view class="summary">
-            <view class="time">10月30号 周一 18:15</view>
-            <view class="other"></view>
-          </view>
-          <view class="share">
+        <view class="content">
 
+          <view class="left">
+            <view class="start address">
+              <view class="icon">
+                <view class="nav">起</view>
+              </view>
+              <view class="text">成武</view>
+            </view>
+            <view class="road address">
+              <view class="icon">
+                <view class="nav">经</view>
+              </view>
+              <text class="text">五岔路口</text>
+            </view>
+            <view class="end address">
+              <view class="icon">
+                <view class="nav">终</view>
+              </view>
+              <view class="text">菏泽</view>
+            </view>
+            <view class="time address">
+              <view class="icon">
+                <view class="nav">时</view>
+              </view>
+              <view class="text">11月12号（周日） 11:12</view>
+            </view>
           </view>
+
+          <view class="right">
+            <view class="summary">
+              <view class="price">
+                <text class="money">12</text>
+                <text class="unit">/人</text>
+              </view>
+              <view class="note">8小时后出发</view>
+            </view>
+            <view class="share">
+              <view class="icon">
+                <text class="fa blue-icon fa-sm fa-share-alt"/>
+              </view>
+            </view>
+          </view>
+        </view>
+
+        <view class="footer">
+          <text class="call-phone">联系TA</text>
         </view>
       </view>
+      <view class="cart">
+        <view class="header">
+          <view class="nav-block"></view>
+          <view class="nav-main">
+            <view class="tag">车找人</view>
+            <view class="seats">剩余2位座</view>
+          </view>
+        </view>
+        <view class="content">
 
-    </view>
+          <view class="left">
+            <view class="start address">
+              <view class="icon">
+                <view class="nav">起</view>
+              </view>
+              <view class="text">成武</view>
+            </view>
+            <view class="end address">
+              <view class="icon">
+                <view class="nav">终</view>
+              </view>
+              <view class="text">济南</view>
+            </view>
+            <view class="time address">
+              <view class="icon">
+                <view class="nav">时</view>
+              </view>
+              <view class="text">11月15号（周一） 08:12</view>
+            </view>
+          </view>
 
-    <!--<button @click="handler">BTN</button>
-    <text class="fa fa-paper-plane-o"></text>
-    <text class="fa fa-paper-plane-o" aria-hidden="true"></text>-->
+          <view class="right">
+            <view class="summary">
+              <view class="price">
+                <text class="money">100</text>
+                <text class="unit">/人</text>
+              </view>
+              <view class="note">16小时后出发</view>
+            </view>
+            <view class="share">
+              <view class="icon">
+                <text class="fa blue-icon fa-sm fa-share-alt"/>
+              </view>
+            </view>
+          </view>
+        </view>
+
+        <view class="footer">
+          <text class="call-phone">联系TA</text>
+        </view>
+      </view>
+      <view class="cart">
+        <view class="header">
+          <view class="nav-block"></view>
+          <view class="nav-main">
+            <view class="tag">车找人</view>
+            <view class="seats">剩余2位座</view>
+          </view>
+        </view>
+        <view class="content">
+
+          <view class="left">
+            <view class="start address">
+              <view class="icon">
+                <view class="nav">起</view>
+              </view>
+              <view class="text">成武</view>
+            </view>
+            <view class="end address">
+              <view class="icon">
+                <view class="nav">终</view>
+              </view>
+              <view class="text">济南</view>
+            </view>
+            <view class="time address">
+              <view class="icon">
+                <view class="nav">时</view>
+              </view>
+              <view class="text">11月15号（周一） 08:12</view>
+            </view>
+          </view>
+
+          <view class="right">
+            <view class="summary">
+              <view class="price">
+                <text class="money">100</text>
+                <text class="unit">/人</text>
+              </view>
+              <view class="note">16小时后出发</view>
+            </view>
+            <view class="share">
+              <view class="icon">
+                <text class="fa blue-icon fa-sm fa-share-alt"/>
+              </view>
+            </view>
+          </view>
+        </view>
+
+        <view class="footer">
+          <text class="call-phone">联系TA</text>
+        </view>
+      </view>
+    </scroll-view>
+
   </view>
 </template>
 
@@ -140,9 +264,6 @@ export default {
   #index {
     width: 100%;
     height: 100%;
-    /*display: flex;*/
-    /*flex-wrap: wrap;*/
-    /*justify-content: center;*/
   }
   .main {
     height: 725rpx;
@@ -175,7 +296,7 @@ export default {
         background: #ffffff;
         .switch {
           height: 75rpx;
-          width: 240rpx;
+          width: 300rpx;
           display: flex;
           /*background: darkslateblue;*/
           .btn {
@@ -192,7 +313,7 @@ export default {
         }
         .my {
           height: 75rpx;
-          width: 150rpx;
+          width: 200rpx;
           text-align: center;
           line-height: 75rpx;
           /*background: darkgrey;*/
@@ -272,99 +393,218 @@ export default {
       }
     }
   }
+  ::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+    color: transparent;
+  }
   .list {
-    min-height: 700rpx;
+    height: 700rpx;
     width: 100%;
-    margin-top: 20rpx;
+    /*margin-top: 20rpx;*/
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    background: salmon;
+    /*background: darkkhaki;*/
     .cart {
-      height: 240rpx;
-      width: 94%;
-      border-radius: 20rpx;
+      height: 331rpx;
+      width: 100%;
+      border-radius: 10rpx;
       display: flex;
       flex-wrap: wrap;
       align-items: center;
       justify-content: center;
-      background: darkcyan;
-      .map {
-        height: 120rpx;
+      margin-top: 10rpx;
+      background: #ffffff;
+      box-shadow:1rpx 1rpx 5px #E6E6E6;
+      .header {
+        height: 50rpx;
         width: 100%;
-        border-radius: 10rpx;
+        display: flex;
+        padding-top: 10rpx;
+        border-top-left-radius: 10rpx;
+        border-top-right-radius: 10rpx;
+        background: #FAFAFA;
+        .nav-block {
+          height: 35rpx;
+          width: 10rpx;
+          border-radius: 8rpx;
+          margin-left: 15rpx;
+          background: #3B8CDD;
+        }
+        .nav-main {
+          height: 35rpx;
+          display: flex;
+          .tag {
+            height: 35rpx;
+            font-size: 30rpx;
+            line-height: 36rpx;
+            padding-right: 10px;
+            padding-left: 12rpx;
+          }
+          .seats {
+            height: 25rpx;
+            font-size: 24rpx;
+            color: #949494;
+            line-height: 25rpx;
+            text-indent: 15rpx;
+            margin-top: 5rpx;
+            border-left: 4rpx solid #949494;
+          }
+        }
+      }
+      .content {
+        height: 200rpx;
+        width: 100%;
         display: flex;
         flex-wrap: wrap;
         align-items: center;
         justify-content: flex-start;
-        background: darkolivegreen;
-        .run {
-          height: 120rpx;
-          width: 75%;
+        .left {
+          height: 200rpx;
+          width: 70%;
           display: flex;
           flex-wrap: wrap;
           align-items: center;
           justify-content: center;
-          background: darkcyan;
+          /*background: #690e0d;*/
           .address {
-            height: 50%;
+            height: 50rpx;
             width: 100%;
             display: flex;
             flex-wrap: wrap;
             align-items: center;
             justify-content: flex-start;
-            background: darkkhaki;
+            /*background: #fa4d5c;*/
             .icon {
-              height: 100%;
-              width: 15%;
+              height: 50rpx;
+              width: 10%;
               display: flex;
               flex-wrap: wrap;
               align-items: center;
               justify-content: center;
-              background: dimgrey;
+              /*background: darkgoldenrod;*/
+              .nav {
+                height: 30rpx;
+                width: 30rpx;
+                font-size: 20rpx;
+                color: #ffffff;
+                line-height: 30rpx;
+                border-radius: 50%;
+                text-align: center;
+                /*background: darkgray;*/
+              }
             }
             .text {
-              height: 100%;
-              width: 85%;
-              font-size: 36rpx;
+              height: 50rpx;
+              width: 90%;
+              font-size: 32rpx;
+              line-height: 60rpx;
               text-indent: 5rpx;
               display: flex;
               flex-wrap: wrap;
               align-items: center;
               justify-content: flex-start;
-              background: darkgoldenrod;
+              /*background: #58b7ff;*/
+            }
+          }
+          .start {
+            .icon {
+              .nav {
+                background: #3D94FE;
+              }
+            }
+          }
+          .road {
+            .icon {
+              .nav {
+                background: #DADADA;
+              }
+            }
+          }
+          .end {
+            .icon {
+              .nav {
+                background: #C7B685;
+              }
+            }
+          }
+          .time {
+            .icon {
+              .nav {
+                background: #989898;
+              }
+            }
+          }
+          .road {
+            .text {
+              font-size: 28rpx;
+              color: #DADADA;
+            }
+          }
+          .time {
+            .text {
+              font-size: 28rpx;
+              color: #989898;
             }
           }
         }
-        .info {
-          height: 120rpx;
-          width: 25%;
-          font-size: 24rpx;
-          text-align:right;
-          /*margin-right: 10rpx;*/
-          background: firebrick;
-          .price {
-            height: 40rpx;
-            /*width: 100%;*/
-            padding-right: 20rpx;
-            line-height: 40rpx;
-            background: #fa4d5c;
-          }
-          .note {
-            height: 40rpx;
-            /*width: 100%;*/
+        .right {
+          height: 200rpx;
+          width: 30%;
+          .summary {
+            height: 100rpx;
+            font-size: 28rpx;
+            text-align:right;
             line-height: 40rpx;
             padding-right: 20rpx;
-            background: dimgrey;
+            .price {
+              height: 40rpx;
+            }
+            .note {
+              height: 40rpx;
+            }
           }
+          .share {
+            height: 100rpx;
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            justify-content: center;
+            .icon {
+              height: 50rpx;
+              width: 80rpx;
+              line-height: 47rpx;
+              text-align: center;
+              border-radius: 50rpx;
+              background: #F0F0F0;
+            }
+          }
+
         }
       }
-      .guild {
-        height: 120rpx;
+      .footer {
+        height: 79rpx;
         width: 100%;
-        border-radius: 10rpx;
-        background: #58b7ff;
+        display: flex;
+        padding-top: 10rpx;
+        justify-content: flex-end;
+        border-bottom-left-radius: 10rpx;
+        border-bottom-right-radius: 10rpx;
+        border-top: 1rpx solid #EDEDED;
+        .call-phone {
+          height: 50rpx;
+          font-size: 32rpx;
+          line-height: 50rpx;
+          padding: 0 10rpx;
+          margin-right: 15rpx;
+          border-radius: 5rpx;
+          background: #bdb76b;
+        }
       }
     }
   }
+
+
 </style>
