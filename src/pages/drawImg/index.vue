8c114a04e84e203e78188211590b279de10dbde3
chengzhx76 <chengzhx76@qq.com>
2018-11-17 08:30:57 +0800

    增加首页

--- src/pages/drawImg/index.vue
+++ src/pages/drawImg/index.vue
@@ -33,59 +33,48 @@ export default {
     }
   },
   methods: {
-    /**
-     *
-     * @param {CanvasContext} ctx canvas上下文
-     * @param {number} x 圆角矩形选区的左上角 x坐标
-     * @param {number} y 圆角矩形选区的左上角 y坐标
-     * @param {number} w 圆角矩形选区的宽度
-     * @param {number} h 圆角矩形选区的高度
-     * @param {number} r 圆角的半径
-     */
-    roundRect (ctx, x, y, w, h, r) {
-      // 开始绘制
-      ctx.beginPath()
-      // 因为边缘描边存在锯齿，最好指定使用 transparent 填充
-      // 这里是使用 fill 还是 stroke都可以，二选一即可
-      ctx.setFillStyle('transparent')
-      // ctx.setStrokeStyle('transparent')
-      // 左上角
-      ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5)
-
-      // border-top
-      ctx.moveTo(x + r, y)
-      ctx.lineTo(x + w - r, y)
-      ctx.lineTo(x + w, y + r)
-      // 右上角
-      ctx.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2)
-
-      // border-right
-      ctx.lineTo(x + w, y + h - r)
-      ctx.lineTo(x + w - r, y + h)
-      // 右下角
-      ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * 0.5)
-
-      // border-bottom
-      ctx.lineTo(x + r, y + h)
-      ctx.lineTo(x, y + h - r)
-      // 左下角
-      ctx.arc(x + r, y + h - r, r, Math.PI * 0.5, Math.PI)
-
-      // border-left
-      ctx.lineTo(x, y + r)
-      ctx.lineTo(x + r, y)
-
-      // 这里是使用 fill 还是 stroke都可以，二选一即可，但是需要与上面对应
-      ctx.fill()
-      // ctx.stroke()
-      ctx.closePath()
-      // 剪切
-      ctx.clip()
-      ctx.draw()
-    },
     drawImg () {
-      const ctx = wx.createCanvasContext('shareImg')
-      this.roundRect(ctx, 0, 0, 100, 100, 5)
+      let self = this
+      let bg = new Promise(function (resolve, reject) {
+        wx.getImageInfo({
+          src: 'img/bg.png',
+          success: function (res) {
+            resolve(res)
+          }
+        })
+      })
+      let qr = new Promise(function (resolve, reject) {
+        wx.getImageInfo({
+          src: 'img/qr.png',
+          success: function (res) {
+            resolve(res)
+          },
+          fail: function (error) {
+            console.error(error)
+          }
+        })
+      })
+      Promise.all([bg, qr]).then(res => {
+        console.log(res)
+        const ctx = wx.createCanvasContext('shareImg')
+
+        const bgWidth = self.canvasWidthPx
+        const bgHeight = self.canvasHeightPx
+        const qrWidth = 76
+        const qrHeight = 76
+
+        ctx.drawImage('../../' + res[0].path, 0, 0, bgWidth, bgHeight)
+        ctx.drawImage('../../' + res[1].path, (bgWidth - qrWidth) / 2, (bgHeight - qrHeight) / 2, qrWidth, qrHeight)
+
+        ctx.setTextAlign('center')
+        ctx.setFillStyle('red')
+        ctx.setFontSize(18)
+        ctx.fillText('分享文字描述1', bgWidth / 2, 130)
+        ctx.fillText('分享文字描述2', bgWidth / 2, 160)
+
+        ctx.stroke()
+        ctx.draw()
+      })
     },
     createImg () {
       let self = this
