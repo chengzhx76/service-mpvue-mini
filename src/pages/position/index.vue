ef7f1d6dcb9408135da7f7f7ca641c6ca5010d3a
chengzhx76 <chengzhx76@qq.com>
2018-11-25 22:28:11 +0800

    增加首页

---
+++ src/pages/position/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view id="position">
+
+
+  </view>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/styles/mixin.scss";
+  @import "@/styles/variables.scss";
+  #position {
+    @include height-width-100;
+  }
+</style>
