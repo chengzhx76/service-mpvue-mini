b27fc33cbd5e127283ed4a6d728a7431bdfd141a
chengzhx76 <chengzhx76@qq.com>
2018-11-26 14:50:32 +0800

    修改

--- src/pages/position/index.vue
+++ src/pages/position/index.vue
@@ -1,14 +1,13 @@
 <template>
   <view id="position">
     <view class="header">
-      <view class="city">
-        <input placeholder-class="placeholder-color" placeholder="城市名称" v-model=""/>
-      </view>
+      <view class="city">{{ selectCity }}</view>
       <view class="local">
-        <input placeholder-class="placeholder-color" @input="input" placeholder="请输入地点" v-model=""/>
+        <input placeholder-class="placeholder-color" @input="input" placeholder="请输入地点" v-model="selectCity"/>
       </view>
     </view>
     <view class="list">
+
       <view v-for="(position, index) in positions"
             class="position"
             @click="select(position)"
@@ -23,9 +22,14 @@
   export default {
     data () {
       return {
+        selectCity: '呼和浩特市',
+        positions: null
       }
     },
     methods: {
+    },
+    mounted () {
+      console.log(this.$root.$mp.query)
     }
   }
 </script>
@@ -35,5 +39,34 @@
   @import "@/styles/variables.scss";
   #position {
     @include height-width-100;
+    @include column-align-center;
+    background: $white;
+  }
+  .header {
+    @include height-rpx-width-100(110);
+    @include border-top-width(1);
+    @include border-bottom-width(1);
+    @include justify-start-align-center;
+    .city {
+      height: 110rpx;
+      min-width: 20rpx;
+      line-height: 110rpx;
+      padding-left: 15rpx;
+      padding-right: 20rpx;
+      &:after {
+        @include arrow-bottom-left(16, 6, -8);
+      }
+    }
+    .local {
+      height: 110rpx;
+      max-width: 400rpx;
+      @include column-align-center;
+      input {
+        @include height-width-percent-text(80, 100%, left);
+        @include border-left-width(1);
+        margin-top: 15rpx;
+        padding-left: 20rpx;
+      }
+    }
   }
 </style>
