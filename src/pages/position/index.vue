90fd8f3fc75cf3f05780cec60cd9d656ccd90127
chengzhx76 <chengzhx76@qq.com>
2018-11-26 09:16:21 +0800

    修改

--- src/pages/position/index.vue
+++ src/pages/position/index.vue
@@ -1,6 +1,20 @@
 <template>
   <view id="position">
-
+    <view class="header">
+      <view class="city">
+        <input placeholder-class="placeholder-color" placeholder="城市名称" v-model=""/>
+      </view>
+      <view class="local">
+        <input placeholder-class="placeholder-color" @input="input" placeholder="请输入地点" v-model=""/>
+      </view>
+    </view>
+    <view class="list">
+      <view v-for="(position, index) in positions"
+            class="position"
+            @click="select(position)"
+            :key="index"
+            hover-class="choose-hover">{{ position }}</view>
+    </view>
 
   </view>
 </template>
