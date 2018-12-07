8f4fc28cedd9b06e279c3905f97b76f2c5b98978
chengzhx76 <chengzhx76@qq.com>
2018-12-07 23:23:11 +0800

    分享功能待完善

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -9,6 +9,28 @@
          :polyline="polyline"
          show-location
     ></map>
+    <view class="switch">
+      <view class="slider"></view>
+    </view>
+    <view class="nav-text">车找人</view>
+    <view class="service">
+      <view class="header">
+        <view class="user">
+          <view class="avatar">
+            <img src="https://wx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM58ZD0kZT40Kd3KuAfGqefkJ5rf8toThR3UqibPib2PoyAKjfgr6nBRCOxeV0TeibC1pAwICdF2paeFg/132"/>
+          </view>
+          <view class="nickname">
+            Cheng
+          </view>
+        </view>
+        <view class="contact">
+
+        </view>
+      </view>
+      <view class="content">
+
+      </view>
+    </view>
   </view>
 
 </template>
@@ -123,12 +145,68 @@
   @import "@/styles/mixin.scss";
   @import "@/styles/variables.scss";
   #map {
-    @include height-width-100;
-    @include column-align-center;
+    width: 100%;
+    @include justify-align-center;
     background: $white;
   }
   .travel-map {
-    @include height-rpx-width-100(600);
+    @include height-rpx-width-100(500);
+  }
+
+  .switch {
+    @include height-rpx-width-100(50);
+    @include justify-align-center;
+    background: darkorange;
+    .slider {
+      @include height-width(16, 60);
+      @include border-top-bottom-width(5);
+    }
+  }
+  .nav-text {
+    @include height-width-percent-text(80, 100%, left);
+    padding-left: 20rpx;
+    background: darkkhaki;
+  }
+  .service {
+    width: 100%;
+    @include justify-center;
+    background: greenyellow;
+    .header {
+      @include height-rpx-width-percent(119, 96%);
+      @include justify-space-between-align-center;
+      @include border-bottom-width(1);
+      background: firebrick;
+      .user {
+        @include height-100-width-rpx(300);
+        display: flex;
+        background: darkorange;
+        .avatar {
+          @include height-100-width-rpx(100);
+          @include align-center;
+          background: darkgrey;
+          img {
+            @include height-width(100, 100);
+            @include border-radius-percent(50%);
+          }
+        }
+        .nickname {
+          @include height-width-rpx-text-line(60, 200, 80, left);
+          padding-left: 15rpx;
+          font-size: 32rpx;
+          @include over-hidden;
+          background: bisque;
+        }
+      }
+      .contact {
+        @include height-width(80, 180);
+        margin-right: 20rpx;
+        background: darkgoldenrod;
+      }
+    }
+    .content {
+      @include height-rpx-width-percent(400, 96%);
+      background: darkgreen;
+    }
   }
 
 </style>
