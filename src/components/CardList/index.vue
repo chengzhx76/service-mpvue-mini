5deaa420973c9a55b0728f222895f8ba881469e0
chengzhx76 <chengzhx76@qq.com>
2018-12-11 14:35:36 +0800

    更新

--- src/components/CardList/index.vue
+++ src/components/CardList/index.vue
@@ -58,23 +58,14 @@
         <button class="call-phone" size="mini" hover-class="btn-hover" @click.stop="phoneCall(travel.mobileNo)">联系TA</button>
       </view>
     </view>
-    <view style="position: absolute; top: -9999px; left: -9999px;">
-      <canvas canvas-id="shareImg" :style="{height: canvasHeightPx + 'px', width: canvasWidthPx + 'px'}"/>
-    </view>
-    <view class='share-preview' v-if="showShareImg" @click="closeShareImgMode()">
-      <view class="share-warp" :style="{width: canvasWidthPx + 'px'}">
-        <view class="share-img" :style="{height: canvasHeightPx + 'px', width: canvasWidthPx + 'px'}">
-          <image :src='src'/>
-        </view>
-        <view class="save-img-btn" @click="saveShareImg()">保存图片</view>
-      </view>
-    </view>
     <action-sheet ref="actionSheet" @shareImg="createShareImg"></action-sheet>
+    <share-img ref="shareImgCom"></share-img>
   </view>
 </template>
 
 <script>
   import ActionSheet from '@/components/ActionSheet/index'
+  import ShareImg from '@/components/ShareImg/index'
   export default {
     props: {
       travels: {
@@ -85,17 +76,12 @@
     },
     data () {
       return {
-        showShareImg: false,
-        src: '',
-        canvasHeightPx: 260,
-        canvasWidthPx: 200,
-        windowHeightPx: 0,
-        windowWidthPx: 0,
         tempTravel: null
       }
     },
     components: {
-      ActionSheet
+      ActionSheet,
+      ShareImg
     },
     methods: {
       detail (id) {
@@ -112,142 +98,8 @@
         this.$refs.actionSheet.showActionSheet()
       },
       createShareImg () {
-        let val = this.tempTravel
-        wx.showLoading({
-          title: '加载中...'
-        })
-        this.src = ''
-        this.showShareImg = true
-        this.drawImg(val.type, val.origin, val.dest, val.time).then(() => {
-          setTimeout(() => {
-            this.createImg()
-            wx.hideLoading()
-          }, 500)
-        })
-      },
-      saveShareImg () {
-        const self = this
-        wx.saveImageToPhotosAlbum({
-          filePath: self.src,
-          success (res) {
-            self.tempTravel = null
-            self.showShareImg = false
-            wx.showModal({
-              content: '图片已保存到相册',
-              showCancel: false,
-              confirmText: '好的',
-              success (res) {
-                if (res.confirm) {
-                }
-              }
-            })
-          }
-        })
-      },
-      drawImg (type, origin, dest, time) {
-        const self = this
-        let qr = new Promise((resolve, reject) => {
-          wx.getImageInfo({
-            src: '../../img/gh_a53167bbfa26_258.jpg',
-            success (res) {
-              resolve(res)
-            },
-            fail (error) {
-              console.error(error)
-            }
-          })
-        })
-
-        return new Promise((resolve, reject) => {
-          try {
-            Promise.all([qr]).then(res => {
-              const ctx = wx.createCanvasContext('shareImg')
-              ctx.setFillStyle('#F8FCFF')
-              ctx.fillRect(0, 0, self.canvasWidthPx, 170)
-
-              ctx.beginPath()
-              ctx.setFillStyle('#FFFFFF')
-              ctx.fillRect(0, 170, self.canvasWidthPx, 90)
-
-              ctx.beginPath()
-              ctx.setStrokeStyle('#E5E5E5')
-              ctx.setLineWidth(1)
-              ctx.moveTo(10, 170)
-              ctx.lineTo(self.canvasWidthPx - 10, 170)
-              ctx.stroke()
-
-              ctx.beginPath()
-              ctx.setTextAlign('center')
-              ctx.setFillStyle('#26548D')
-              ctx.setFontSize(22)
-              if (type === 1) {
-                ctx.fillText('人找车', self.canvasWidthPx * 0.5, 35)
-              } else {
-                ctx.fillText('车找人', self.canvasWidthPx * 0.5, 35)
-              }
-              ctx.beginPath()
-              ctx.setTextAlign('right')
-              ctx.setFillStyle('#26548D')
-              ctx.setFontSize(18)
-              ctx.fillText('起点：', self.canvasWidthPx * 0.25, 70)
-              ctx.fillText('终点：', self.canvasWidthPx * 0.25, 100)
-              ctx.fillText('时间：', self.canvasWidthPx * 0.25, 130)
-
-              ctx.beginPath()
-              ctx.setTextAlign('left')
-              ctx.setFillStyle('#0A1519')
-              ctx.setFontSize(18)
-              ctx.fillText(origin, self.canvasWidthPx * 0.25, 70)
-              ctx.fillText(dest, self.canvasWidthPx * 0.25, 100)
-              ctx.fillText(time, self.canvasWidthPx * 0.25, 130)
-
-              ctx.beginPath()
-              ctx.setTextAlign('left')
-              ctx.setFillStyle('#ACACAC')
-              ctx.setFontSize(14)
-              ctx.fillText('长按识别小程序码，联系TA', self.canvasWidthPx * 0.08, 200)
-              ctx.fillText('分享来自「成武拼车」', self.canvasWidthPx * 0.08, 226)
-
-              ctx.drawImage('../../' + res[0].path, self.canvasWidthPx - 80, 180, 70, 70)
-              ctx.draw()
-              resolve()
-            })
-          } catch (error) {
-            console.log(error)
-            reject(error)
-          }
-        })
-      },
-      createImg () {
-        let self = this
-        wx.canvasToTempFilePath({
-          x: 0,
-          y: 0,
-          width: self.canvasWidthPx,
-          height: self.canvasHeightPx,
-          destWidth: self.canvasWidthPx * 2,
-          destHeight: self.canvasHeightPx * 2,
-          canvasId: 'shareImg',
-          success (res) {
-            self.src = res.tempFilePath
-          },
-          fail (res) {
-            console.log(res)
-          }
-        })
-      },
-      closeShareImgMode () {
-        this.tempTravel = null
-        this.showShareImg = false
+        this.$refs.shareImgCom.createShareImg(this.tempTravel)
       }
-    },
-    mounted () {
-      const res = wx.getSystemInfoSync()
-      const clientHeight = res.windowHeight
-      const clientWidth = res.windowWidth
-      this.windowHeightPx = clientHeight
-      this.windowWidthPx = clientWidth
-      this.canvasWidthPx = Math.ceil(clientWidth * 0.9)
     }
   }
 </script>
@@ -428,32 +280,6 @@
         color: $cardBtnFontColor;
         padding: 0 15rpx;
         margin-right: 15rpx;
-      }
-    }
-  }
-  .share-preview {
-    @include height-width-100;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 999;
-    background:rgba(0, 0, 0, 0.5);
-    @include justify-align-center;
-    .share-warp {
-      @include column-align-center;
-      .share-img {
-        image {
-          @include height-width-100;
-          @include border-radius(5);
-        }
-      }
-      .save-img-btn {
-        @include height-width-text-center(80, 240)
-        @include border-radius(30);
-        margin-top: 50rpx;
-        color: #446C9D;
-        font-size: 32rpx;
-        background: #ffffff;
       }
     }
   }
---
+++ src/components/ShareImg/index.vue
@@ -0,0 +1,245 @@
+<template>
+  <view id="share-img">
+    <view style="position: absolute; top: -9999px; left: -9999px;">
+      <canvas canvas-id="shareImg" :style="{height: canvasHeightPx + 'px', width: canvasWidthPx + 'px'}"/>
+    </view>
+    <view class='share-preview' v-if="showShareImg" @click="closeShareImgMode()" catchtouchmove='true'>
+      <view class="share-warp" :style="{width: canvasWidthPx + 'px'}">
+        <view class="share-img" :style="{height: canvasHeightPx + 'px', width: canvasWidthPx + 'px'}">
+          <image :src='src'/>
+        </view>
+        <view class="save-img-btn" @click="saveShareImg()">保存图片</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+    },
+    data () {
+      return {
+        showShareImg: false,
+        showImg: true,
+        src: '',
+        canvasHeightPx: 260,
+        canvasWidthPx: 200,
+        windowHeightPx: 0,
+        windowWidthPx: 0
+      }
+    },
+    methods: {
+      createShareImg (val, showImg) {
+        if (showImg !== null && showImg !== undefined) {
+          this.showImg = showImg
+        }
+        if (this.showImg) {
+          this.src = ''
+          wx.showLoading({
+            title: '加载中...'
+          })
+          this.showShareImg = true
+        }
+        this.drawImg(val.type, val.origin, val.dest, val.time, val.mobileNo).then(() => {
+          setTimeout(() => {
+            this.createImg()
+            if (this.showImg) {
+              wx.hideLoading()
+            }
+          }, 500)
+        })
+      },
+      drawImg (type, origin, dest, time, mobileNo) {
+        const self = this
+        let bg = new Promise((resolve, reject) => {
+          wx.getImageInfo({
+            src: 'https://chengzhx76.picp.vip/share-bg.jpg',
+            success (res) {
+              resolve(res)
+            },
+            fail (error) {
+              console.error(error)
+            }
+          })
+        })
+        let qr = new Promise((resolve, reject) => {
+          wx.getImageInfo({
+            src: '../../img/gh_a53167bbfa26_258.jpg',
+            success (res) {
+              resolve(res)
+            },
+            fail (error) {
+              console.error(error)
+            }
+          })
+        })
+
+        return new Promise((resolve, reject) => {
+          try {
+            Promise.all([bg, qr]).then(res => {
+              const ctx = wx.createCanvasContext('shareImg')
+              // 上部 view
+              // ctx.setFillStyle('#F8FCFF')
+              // ctx.fillRect(0, 0, self.canvasWidthPx, 170)
+
+              if (self.showImg) {
+                ctx.drawImage(res[0].path, 0, 0, self.canvasWidthPx, 170)
+
+                // 底部view
+                ctx.beginPath()
+                ctx.setFillStyle('#FFFFFF')
+                ctx.fillRect(0, 170, self.canvasWidthPx, 90)
+              } else {
+                ctx.drawImage(res[0].path, 0, 0, self.canvasWidthPx, 260)
+              }
+
+              // 中间的灰线
+              // ctx.beginPath()
+              // ctx.setStrokeStyle('#E5E5E5')
+              // ctx.setLineWidth(1)
+              // ctx.moveTo(10, 170)
+              // ctx.lineTo(self.canvasWidthPx - 10, 170)
+              // ctx.stroke()
+
+              // 头部信息
+              ctx.beginPath()
+              ctx.setTextAlign('center')
+              ctx.setFillStyle('#26548D')
+              ctx.setFontSize(22)
+              if (type === 1) {
+                ctx.fillText('人找车', self.canvasWidthPx * 0.5, 35)
+              } else {
+                ctx.fillText('车找人', self.canvasWidthPx * 0.5, 35)
+              }
+
+              // 行程信息
+              ctx.beginPath()
+              ctx.setTextAlign('right')
+              ctx.setFillStyle('#26548D')
+              ctx.setFontSize(18)
+              ctx.fillText('起点：', self.canvasWidthPx * 0.25, 70)
+              ctx.fillText('终点：', self.canvasWidthPx * 0.25, 100)
+              ctx.fillText('时间：', self.canvasWidthPx * 0.25, 130)
+              if (!self.showImg) {
+                ctx.fillText('手机：', self.canvasWidthPx * 0.25, 160)
+              }
+
+              ctx.beginPath()
+              ctx.setTextAlign('left')
+              ctx.setFillStyle('#0A1519')
+              ctx.setFontSize(18)
+              ctx.fillText(origin, self.canvasWidthPx * 0.25, 70)
+              ctx.fillText(dest, self.canvasWidthPx * 0.25, 100)
+              ctx.fillText(time, self.canvasWidthPx * 0.25, 130)
+              if (!self.showImg) {
+                ctx.fillText(mobileNo, self.canvasWidthPx * 0.25, 160)
+              }
+              if (self.showImg) {
+                // 底部信息
+                ctx.beginPath()
+                ctx.setTextAlign('left')
+                ctx.setFillStyle('#ACACAC')
+                ctx.setFontSize(14)
+                ctx.fillText('长按识别小程序码，联系TA', self.canvasWidthPx * 0.08, 200)
+                ctx.fillText('分享来自「成武拼车」', self.canvasWidthPx * 0.08, 226)
+
+                // 小程序码
+                ctx.drawImage('../../' + res[1].path, self.canvasWidthPx - 80, 180, 70, 70)
+              }
+              ctx.draw()
+              resolve()
+            })
+          } catch (error) {
+            console.log(error)
+            reject(error)
+          }
+        })
+      },
+      createImg () {
+        let self = this
+        wx.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width: self.canvasWidthPx,
+          height: self.canvasHeightPx,
+          destWidth: self.canvasWidthPx * 2,
+          destHeight: self.canvasHeightPx * 2,
+          canvasId: 'shareImg',
+          success (res) {
+            if (self.showImg) {
+              self.src = res.tempFilePath
+            } else {
+              self.$emit('shareImgUrl', res.tempFilePath)
+            }
+          },
+          fail (res) {
+            console.log(res)
+          }
+        })
+      },
+      closeShareImgMode () {
+        this.showShareImg = false
+      },
+      saveShareImg () {
+        const self = this
+        wx.saveImageToPhotosAlbum({
+          filePath: self.src,
+          success (res) {
+            self.showShareImg = false
+            wx.showModal({
+              content: '图片已保存到相册',
+              showCancel: false,
+              confirmText: '好的',
+              success (res) {
+                if (res.confirm) {
+                }
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted () {
+      const res = wx.getSystemInfoSync()
+      const clientHeight = res.windowHeight
+      const clientWidth = res.windowWidth
+      this.windowHeightPx = clientHeight
+      this.windowWidthPx = clientWidth
+      this.canvasWidthPx = Math.ceil(clientWidth * 0.9)
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/styles/mixin.scss";
+  @import "@/styles/variables.scss";
+  #share-img {
+  }
+  .share-preview {
+    @include height-width-100;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    background:rgba(0, 0, 0, 0.5);
+    @include justify-align-center;
+    .share-warp {
+      @include column-align-center;
+      .share-img {
+        image {
+          @include height-width-100;
+          @include border-radius(5);
+        }
+      }
+      .save-img-btn {
+        @include height-width-text-center(80, 240);
+        @include border-radius(30);
+        margin-top: 50rpx;
+        color: #446C9D;
+        font-size: 32rpx;
+        background: #ffffff;
+      }
+    }
+  }
+</style>
