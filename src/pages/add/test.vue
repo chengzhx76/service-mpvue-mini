8d40e7f4f135b05d48f04e73e1f27fd7685d1624
chengzhx76 <chengzhx76@qq.com>
2018-12-20 22:53:00 +0800

    分享功能待完善

---
+++ src/pages/add/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <view id="test">
+    <choose-input clazz="origin" icon="fa-car" labelText="起点" validate="true" placeholder="请输入起点" content="成武" @choose="chooseOrigin"/>
+
+    <!--<button @click="showModal">点我</button>-->
+  </view>
+</template>
+
+<script>
+  import ChooseInput from './components/ChooseInput'
+  export default {
+    data () {
+      return {
+      }
+    },
+    components: {
+      ChooseInput
+    },
+    methods: {
+      chooseOrigin () {
+        console.log('---------------')
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/styles/mixin.scss";
+  @import "@/styles/variables.scss";
+  #test {
+    width: 100%;
+    height: 100%;
+  }
+</style>
