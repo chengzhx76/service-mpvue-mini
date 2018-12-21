df76f624a504b4b9db210251130020836b068070
chengzhx76 <chengzhx76@qq.com>
2018-12-21 10:11:58 +0800

    更新

--- src/pages/add/components/ChooseInput.vue
+++ src/pages/add/components/ChooseInput.vue
@@ -8,7 +8,7 @@
         <text :class="['label', { error: validate }]">{{ labelText }}</text>
       </view>
       <view class="input">
-        <input placeholder-class="placeholder-color" :placeholder="placeholder" disabled v-model="content"/>
+        <input placeholder-class="placeholder-color" :placeholder="placeholder" disabled v-model="location.title"/>
       </view>
     </view>
   </view>
@@ -17,43 +17,66 @@
 <script>
   export default {
     props: {
-      clazz: {
-        type: String,
+      type: {
+        type: Number,
         default: ''
-      },
-      icon: {
-        type: String,
-        default: 'fa-car'
-      },
-      labelText: {
-        type: String,
-        default: '11'
-      },
-      validate: {
-        type: Boolean,
-        default: true
-      },
-      placeholder: {
-        type: String,
-        default: '请11'
-      },
-      content: {
-        type: String,
-        default: '起点！！！'
       }
     },
     data () {
       return {
+        validate: true,
+        location: {
+          title: '',
+          lat: '',
+          lng: ''
+        }
       }
     },
     methods: {
       // 只需要把值给我，所以在这里把值传给我就行了
       choose () {
-        this.$emit('choose')
+        const posType = this.type === 'origin' ? '1' : '2'
+        const url = `../position/main?posType=${posType}`
+        wx.navigateTo({ url })
+      },
+      getAddress () {
+        this.$emit('address', this.location)
       },
       confirm () {
       }
-    }
+    },
+    computed: {
+      clazz () {
+        return this.type === 'origin' ? 'origin' : 'dest'
+      },
+      icon () {
+        return this.type === 'origin' ? 'fa-car' : 'fa-map-marker'
+      },
+      labelText () {
+        return this.type === 'origin' ? '起点' : '终点'
+      },
+      placeholder () {
+        return this.type === 'origin' ? '请选择起点' : '请选择终点'
+      },
+      placeholder () {
+        return this.type === 'origin' ? '请选择起点' : '请选择终点'
+      }
+    },
+    onShow () {
+      if (this.$mp.page.data && this.$mp.page.data.extend && this.$mp.page.data.extend.position) {
+        // const posType = this.$mp.page.data.extend.posType
+        const title = this.$mp.page.data.extend.position.title
+        const { lat, lng } = this.$mp.page.data.extend.position.location
+        this.location = {
+          title: title,
+          lat: lat,
+          lng: lng
+        }
+        this.$mp.page.setData({
+          extend: null
+        })
+      }
+    },
   }
 </script>
 
