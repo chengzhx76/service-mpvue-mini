83fd4813d89db592e522cc775ec083e71ad7787d
chengzhx76 <chengzhx76@qq.com>
2018-12-30 00:19:31 +0800

    分享功能待完善

---
+++ src/pages/test/index.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <view id="test">
+    <map class="map" show-location></map>
+    <center-dialog></center-dialog>
+  </view>
+
+</template>
+
+<script>
+  import CenterDialog from '@/components/CenterDialog/index'
+  export default {
+    data () {
+      return {
+      }
+    },
+    components: {
+      CenterDialog
+    },
+    methods: {
+    },
+    onLoad () {
+    },
+    onUnload () {
+    },
+    created () {
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "@/styles/mixin.scss";
+  @import "@/styles/variables.scss";
+  #test {
+    @include height-width-100;
+    background: $white;
+  }
+  .map {
+    height: 500rpx;
+    width: 100%;
+  }
+
+
+</style>
