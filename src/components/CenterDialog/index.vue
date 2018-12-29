9ceff5e08a1c9c28d0498282bc41f0a1909e8121
chengzhx76 <chengzhx76@qq.com>
2018-12-30 00:19:31 +0800

    分享功能待完善

--- src/components/CenterDialog/index.vue
+++ src/components/CenterDialog/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <view id="center-dialog">
-    <view class="modals modals-center-dialog" v-if="!hidden" catchtouchmove='true'>
-      <view class="modals-cancel" @click="hideModal"></view>
-
-      <view class="center-dialog-body center-pos">
-        <view class="main">
-        </view>
-      </view>
-
-
-    </view>
-  </view>
+    <cover-view class="modal-dialog" v-if="!hidden">
+      <cover-view class="modal-body">
+        <cover-view class="title">{{ title }}</cover-view>
+        <cover-view class="content">
+          {{ content }}
+        </cover-view>
+        <cover-view class="footer">
+          <cover-view class="btn cancel" hover-class="btn-hover">{{ cancelText }}</cover-view>
+          <cover-view class="btn confirm" hover-class="btn-hover">{{ confirmText }}</cover-view>
+        </cover-view>
+      </cover-view>
+    </cover-view>
 </template>
 
 <script>
@@ -19,7 +19,11 @@
     },
     data () {
       return {
-        hidden: true
+        hidden: false,
+        title: '提示',
+        content: '内容',
+        cancelText: '取消',
+        confirmText: '确定'
       }
     },
     methods: {
@@ -38,35 +42,47 @@
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import "@/styles/mixin.scss";
   @import "@/styles/variables.scss";
-  #bottom-dialog {
-  }
-  /*模态框*/
-  .modals {
+  .modal-dialog {
     position: fixed;
     z-index: 999;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
+    background:rgba(0, 0, 0, 0.5);
+    @include justify-align-center;
   }
-  .modals-cancel {
-    position: absolute;
-    z-index: 1000;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: black;
-  }
-  .center-dialog-body {
-    position: absolute;
-    z-index: 10001;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: #fff;
-    .main {
+  .modal-body {
+    width: 75%;
+    background: $white;
+    @include border-radius(6);
+    .title {
+      @include height-width-percent-text-center(80);
+      font-size: 36rpx;
+    }
+    .content {
+      height: 200rpx;
       width: 100%;
+      @include border-top-width(1);
+    }
+    .footer {
+      width: 100%;
+      @include justify-space-between-align-center;
+      @include border-top-width(1);
+      background: $borderColor;
+      .cancel {
+        width: calc(50% - 1rpx);
+        border-bottom-left-radius: 6rpx;
+      }
+      .confirm {
+        width: 50%;
+        border-bottom-right-radius: 6rpx;
+      }
+    }
+    .btn {
+      @include height-line-center(80);
+      font-size: 38rpx;
+      background: $white;
     }
   }
 </style>
